92b9c77178aab24dfc15e8eec53960467cd34319
icgswift <chengjianfreedom@163.com>
2020-06-01 20:37:35 +0800

    第一次状态提升的原因

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Squares extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null,
+            xIsNext: true
+        }
+    }
+    render() {
+        return (
+            <div>
+                <button
+                    className="square"
+                    // 只能修改子组件自身的状态 影响不到其他子组件的状态
+                    onClick={() => this.setState({ value: this.state.xIsNext ? 'x' : 'o', xIsNext: !this.state.xIsNext })}>
+                    {this.state.value}
+                </button>
+            </div>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//状态提升 将子组件的state交给父组件管理
+//第一次状态提升：子组件中只能管理各自状态，这里子组件之间的状态互相影响
+class Board extends Component {
+
+    render() {
+        return (
+            <div>
+                <div className='board-row'>
+                    <Squares value={0} />
+                    <Squares value={1} />
+                    <Squares value={2} />
+                </div>
+                <div className='board-row'>
+                    <Squares value={3} />
+                    <Squares value={4} />
+                    <Squares value={5} />
+                </div>
+                <div className='board-row'>
+                    <Squares value={6} />
+                    <Squares value={7} />
+                    <Squares value={8} />
+                </div>
+            </div >
+        )
+    }
+}
+
+class Game extends Component {
+
+    render() {
+        return (
+            <div>
+                <Board />
+            </div>
+        )
+    }
+}
+
+render(<Game />, document.getElementById('root'))
+
+
+
