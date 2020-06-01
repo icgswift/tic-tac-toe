dcec03fb6fc4466b82d2db91089cb83544ded02b
icgswift <chengjianfreedom@163.com>
2020-06-01 23:22:00 +0800

    判断胜者， 不可变性，setstate后数据的获取

--- src/index.js
+++ src/index.js
@@ -31,8 +31,15 @@ class Board extends Component {
     }
 
     handleClick(i) {
-        const value = this.state.value.slice()    //不要修改原数据
-        if (value[i]) {
+        const value = this.state.value.slice()
+
+        /*不要直接修改原数据（!this.state.xIsNext同理）     注意下面修改value时(value[i]) 与this.state.value没有关联
+                        1.简化复杂的功能
+                        2.跟踪数据的改变
+                        3.确定在Reac 中何时重新渲染  构建pure components
+                                                                                            */
+        //两种情况下阻止点击响应   1、一方获胜后 再点击时   2、位置已落子 再次落子
+        if (calculateWinner(this.state.value) || value[i]) {
             return
         }
         value[i] = this.state.xIsNext ? 'x' : 'o'
@@ -40,6 +47,8 @@ class Board extends Component {
             value,
             xIsNext: !this.state.xIsNext
         })
+
+        // calculateWinner(this.state.value)    注意此时拿不到setstate后的值
     }
 
     renderSquares(i) {
@@ -51,13 +60,19 @@ class Board extends Component {
     }
 
     render() {
-        let status = `now player:${this.state.xIsNext ? 'X' : 'O'}`
+        // render函数中可以确定this.setstate已执行完毕，此时可以得到setstate后的数据,在渲染DOM之前
+        let status
+        const winner = calculateWinner(this.state.value)
+        if (winner) {
+            status = `WINNER:${winner}`
+        } else {
+            status = `now player:${this.state.xIsNext ? 'X' : 'O'}`
+        }
+        // debugger
         return (
             <div>
                 <h1>{status}</h1>
                 <div className='board-row'>
-                    {/* <Squares value={this.state.value} onclick={() => this.handleClick()} /> */}
-                    {/* <Squares value={this.state.value} onclick={() => this.handleClick()} /> */}
                     {this.renderSquares(0)}
                     {this.renderSquares(1)}
                     {this.renderSquares(2)}
@@ -91,4 +106,26 @@ class Game extends Component {
 render(<Game />, document.getElementById('root'))
 
 
+// 定义判断胜者的函数   在状态变化后将数组传递进函数进行判断
+
+function calculateWinner(value) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    // 思路：将所有获胜条件和状态数组进行比对，满足其一
+    for (var i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (value[a] && value[a] === value[b] && value[a] === value[c]) {
+            return value[a]
+        }
+    }
+    return
+}
 
